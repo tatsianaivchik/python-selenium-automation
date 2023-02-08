b44d9732d2cc049c49ec58e658d92ab23f839c62
tatsianaivchik <tatsianaivchik@gmail.com>
2023-02-08 10:36:26 -0800

    updated homework

--- HWLocators.py
+++ HWLocators.py
@@ -6,18 +6,23 @@ service = Service('/Users/TANYA/QA_Automation/python-selenium-automation/chromed
 driver = webdriver.Chrome(service=service)
 
 
-driver.find_element(By.XPATH, "//i[@aria-label='Amazon']")
-driver.find_element(By.ID, "ap_email")
-driver.find_element(By.ID, "continue")
-driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
-driver.find_element(By.ID, "auth-fpp-link-bottom")
-driver.find_element(By.ID, "ap-other-signin-issues-link")
-driver.find_element(By.ID, "createAccountSubmit")
-driver.find_element(By.XPATH, "//a[contains(@href, 'ap_signin_notification_condition_of_use')]")
-driver.find_element(By.XPATH, "//a[contains(@href, 'ap_signin_notification_privacy_notice')]")
+# driver.find_element(By.XPATH, "//i[@aria-label='Amazon']")
+# driver.find_element(By.ID, "ap_email")
+# driver.find_element(By.ID, "continue")
+# driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+# driver.find_element(By.ID, "auth-fpp-link-bottom")
+# driver.find_element(By.ID, "ap-other-signin-issues-link")
+# driver.find_element(By.ID, "createAccountSubmit")
+# driver.find_element(By.XPATH, "//a[contains(@href, 'ap_signin_notification_condition_of_use')]")
+# driver.find_element(By.XPATH, "//a[contains(@href, 'ap_signin_notification_privacy_notice')]")
 
 
+driver.get('https://www.amazon.com')
 
+driver.find_element(By.ID, "nav-orders").click()
+expected_result = "Sign in"
+actual_result = driver.find_element(By.XPATH, "//h1[@class='a-spacing-small']").text
 
+assert expected_result == actual_result, f'Expected {expected_result} but got {actual_result}'
 
-
+driver.quit()
